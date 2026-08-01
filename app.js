f4600b07b7f552053a450013fa7eb4ba56875405
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-08-01 14:51:16 +0600

    Add click event listener to calculate age

---
+++ app.js
@@ -0,0 +1,39 @@
+document.getElementById('container').innerHTML = `
+    <h1>Calculate Your Age</h1>
+    <label for="date">Date Of Bath:</label>
+    <input type="date" class="date" id="date">
+    <button id="calculateBtn">Calculate Age</button>
+    <div id="result">
+    </div>
+`;
+
+const date = document.getElementById('date');
+const result = document.getElementById('result');
+const calculateBtn = document.getElementById("calculateBtn");
+
+calculateBtn.addEventListener('click', () => {
+    if (!date.value) {
+        result.innerText = "Please select your Birth.";
+        result.classList.add('result');
+        return;
+    }
+    const birthDate = new Date(date.value);
+    const today = new Date();
+
+    let years = today.getFullYear() - birthDate.getFullYear()
+    let month = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+        month--;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    if (month < 0) {
+        years--;
+        month += 12;
+    }
+    if (years < 0) years = 0;
+    result.innerText = `Your Age: ${years} years ${month} months ${days} days.`;
+    result.classList.add('result');
+})
